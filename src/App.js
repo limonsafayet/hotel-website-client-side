d9ecfe6c562263849cff4180be7f5b9deda1b70c
limonsafayet <limon6531@gmail.com>
2021-10-30 15:56:35 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Header from './components/Header/Header';
+import AuthProvider from './contexts/AuthProvider';
+import Home from './pages/Home/Home';
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
+    </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import useAuth from '../../hooks/useAuth';
+import logo from "../../assets/logo.png"
 
 function Header() {
+    const { user, logOut } = useAuth();
     return (
-        <div>
-
-        </div>
+        <>
+            <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg" >
+                <Container>
+                    <Navbar.Brand as={Link} to="/"><img src={logo} alt="luxury hotels" /> LUXURY HOTEL</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        {user?.email ?
+                            <Button onClick={logOut} variant="light">Logout</Button> :
+                            <Nav.Link as={Link} to="/login"><Button variant="outline-light">Login</Button></Nav.Link>}
+                        {user?.displayName ?
+                            <Navbar.Text className="ms-3">
+                                Signed in as: <span className="text-light">{user?.displayName}</span>
+                            </Navbar.Text> : ""
+                        }
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
