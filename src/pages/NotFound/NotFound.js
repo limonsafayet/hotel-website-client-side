0abad54367c23e0fd565e6a881a3d0312d4336da
limonsafayet <limon6531@gmail.com>
2021-10-31 18:19:40 +0600

    notfound

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -6,8 +6,8 @@ function NotFound() {
         <Container>
             <Row>
                 <Col xs={12} md={6} className="mx-auto mt-5 mb-5 text-center">
-                    <h1><b>404</b></h1>
-                    <h3>Sorry Page Not Found</h3>
+                    <img src="http://www.404.ie/assets/img/logo_blue.png" alt="404" className="mb-3 img-fluid" />
+                    <h1>Sorry Page Not Found</h1>
                 </Col>
             </Row>
         </Container>
