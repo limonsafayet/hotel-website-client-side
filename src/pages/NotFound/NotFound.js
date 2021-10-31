9e009b8f94df473110839d4e4e7887bd1a4da7d0
limonsafayet <limon6531@gmail.com>
2021-10-31 18:20:07 +0600

    notfound

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap'
 function NotFound() {
     return (
         <Container>
-            <Row>
+            <Row className="mb-5">
                 <Col xs={12} md={6} className="mx-auto mt-5 mb-5 text-center">
                     <img src="http://www.404.ie/assets/img/logo_blue.png" alt="404" className="mb-3 img-fluid" />
                     <h1>Sorry Page Not Found</h1>
